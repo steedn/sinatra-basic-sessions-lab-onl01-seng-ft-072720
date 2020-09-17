5b610954c95fdef8525dd2cb68e4141fac8428c6
Nathan Steed <steedn@outlook.com>
2020-09-17 20:33:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :sessions 
+    set :session_secret, "77724board"
+  end
+  get '/' do
+    erb :index 
+  end
 end
